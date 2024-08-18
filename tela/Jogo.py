79dff38d5dd922198022178968f66a4cfa06b371
Ieleny <ielenyfilgueira@hotmail.com>
2024-08-18 13:56:27 -0300

    feat: Criado a função de cliqueBolas

--- tela/Jogo.py
+++ tela/Jogo.py
@@ -4,7 +4,10 @@ from random import *
 from .CoresModel import CoresModel
 
 class Jogo:
-    
+
+    bola_um = pygame.Rect(780, 240, 300, 300)
+    ultimaBolaTorre = []
+
     def __init__(self, TELA):
         self.TELA = TELA
 
@@ -22,7 +25,7 @@ class Jogo:
         # Torre horizontal
         pygame.draw.rect(self.TELA, (0, 0, 0, 0), [100, 470, 750, 100])
         self.gerarBolasHorizontal(190,520,40)
-
+        
         pygame.display.flip()
 
     def gerarTorresVerticais(self, X_RECT, X_BOLAS):
@@ -33,23 +36,24 @@ class Jogo:
             [X_RECT, 50, 150, 400]
         )
         
-        self.gerarBolasVertical(X_BOLAS, 120, 50)
+        self.gerarBolasVertical(X_BOLAS, 120, 40)
 
     def gerarBolasVertical(self, X, Y, diametro):
         
-        for item in range(5):
-            # TODO: Quando clicar irá adicionar a bordar verde
-            pygame.draw.circle(self.TELA, CoresModel.VERDE, [X, Y], diametro + 5)
-            pygame.draw.circle(self.TELA, CoresModel.VERMELHO, [X, Y], diametro)
+        for item in range(3):
 
+            NUMERO_ATUAL = str(randrange(10))
+            self.ultimaBolaTorre.append(NUMERO_ATUAL)
+
+            pygame.draw.circle(self.TELA, CoresModel.VERMELHO, [X, Y], diametro)
             self.TELA.blit(
-                pygame.font.SysFont('Comic Sans MS', 40).render(str(randrange(10)), False, (0, 0, 0)),
+                pygame.font.SysFont('Comic Sans MS', 40).render(NUMERO_ATUAL, False, (0, 0, 0)),
                 (X-10,Y-25)
             )
+
             Y += 120
 
     def gerarBolasHorizontal(self, X, Y, diametro):
-        
         for item in range(6):
             pygame.draw.circle(
                 self.TELA, 
@@ -62,3 +66,12 @@ class Jogo:
                 (X-10,Y-25)
             )
             X += 120
+
+    def cliqueBolas(self, event):
+        # Verifica se a posição é do botão
+        if self.bola_um.collidepoint(event.pos):
+            # TODO: Quando clicar irá adicionar a bordar verde
+            pygame.draw.circle(self.TELA, CoresModel.VERDE, [780, 360], 55)
+            pygame.draw.circle(self.TELA, CoresModel.VERMELHO, [400, 480], 40)
+
+            print(self.ultimaBolaTorre)
