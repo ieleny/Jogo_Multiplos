68fc7c1398f9989c523f529f2fede95f8a3b72cb
Ieleny <ielenyfilgueira@hotmail.com>
2024-08-23 18:11:21 -0300

    feat: Seleciona a bola e marca com uma borda verde

--- tela/Jogo.py
+++ tela/Jogo.py
@@ -75,21 +75,39 @@ class Jogo:
     def cliqueBolas(self, event):
             # Verifica se a posição é do botão
             if self.listaBolasClicaveis[2].collidepoint(event.pos):
-                pygame.draw.circle(self.TELA, CoresModel.VERDE, [self.ultimaBolaTorre[2].Altura, self.ultimaBolaTorre[2].Largura], 55)
+                pygame.draw.circle(self.TELA, CoresModel.VERDE, [self.ultimaBolaTorre[2].Altura, self.ultimaBolaTorre[2].Largura], 45)
+                pygame.draw.circle(
+                    self.TELA, 
+                    CoresModel.VERMELHO, 
+                    [self.ultimaBolaTorre[2].Altura, self.ultimaBolaTorre[2].Largura], 
+                    40
+                )
                 self.TELA.blit(
                     pygame.font.SysFont('Comic Sans MS', 40).render(str(self.ultimaBolaTorre[2].NumeroBola), False, (0, 0, 0)),
                     (self.ultimaBolaTorre[2].Altura - 10, self.ultimaBolaTorre[2].Largura - 25)
                 )
 
             if self.listaBolasClicaveis[5].collidepoint(event.pos):
-                pygame.draw.circle(self.TELA, CoresModel.VERDE, [self.ultimaBolaTorre[5].Altura, self.ultimaBolaTorre[5].Largura], 55)
+                pygame.draw.circle(self.TELA, CoresModel.VERDE, [self.ultimaBolaTorre[5].Altura, self.ultimaBolaTorre[5].Largura], 45)
+                pygame.draw.circle(
+                    self.TELA, 
+                    CoresModel.VERMELHO, 
+                    [self.ultimaBolaTorre[5].Altura, self.ultimaBolaTorre[5].Largura], 
+                    40
+                )
                 self.TELA.blit(
                     pygame.font.SysFont('Comic Sans MS', 40).render(str(self.ultimaBolaTorre[5].NumeroBola), False, (0, 0, 0)),
                     (self.ultimaBolaTorre[5].Altura - 10, self.ultimaBolaTorre[5].Largura - 25)
                 )
             
             if self.listaBolasClicaveis[8].collidepoint(event.pos):
-                pygame.draw.circle(self.TELA, CoresModel.VERDE, [self.ultimaBolaTorre[8].Altura, self.ultimaBolaTorre[8].Largura], 55)
+                pygame.draw.circle(self.TELA, CoresModel.VERDE, [self.ultimaBolaTorre[8].Altura, self.ultimaBolaTorre[8].Largura], 45)
+                pygame.draw.circle(
+                    self.TELA, 
+                    CoresModel.VERMELHO, 
+                    [self.ultimaBolaTorre[8].Altura, self.ultimaBolaTorre[8].Largura], 
+                    40
+                )
                 self.TELA.blit(
                     pygame.font.SysFont('Comic Sans MS', 40).render(str(self.ultimaBolaTorre[8].NumeroBola), False, (0, 0, 0)),
                     (self.ultimaBolaTorre[8].Altura - 10, self.ultimaBolaTorre[8].Largura - 25)
