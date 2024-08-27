4f08e9ebca4fd1d2bac85c87d16068531cd838a5
Ieleny <ielenyfilgueira@hotmail.com>
2024-08-26 21:44:16 -0300

    feat: Modificar o nome da classe de ModeloTorresVerticais para ModeloTorres

---
+++ tela/ModeloTorres.py
@@ -0,0 +1,11 @@
+class ModeloTorres:
+    TipoTorre: 0
+    NumeroBola: 0
+    EixoX: 0
+    EixoY: 0
+
+    def __init__(self, TipoTorre, NumeroBola, EixoX, EixoY):
+        self.TipoTorre = TipoTorre
+        self.NumeroBola = NumeroBola
+        self.EixoX = EixoX
+        self.EixoY = EixoY
